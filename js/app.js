d4b362b860e91d2b311377f17571188fdea8f718
oreynoldson <oli.reynoldson@gmail.com>
2014-08-27 16:33:02 -0700

    Added a for loop and prompt
    Added a for loop to check the input provided in the prompt function.

--- js/app.js
+++ js/app.js
@@ -4,18 +4,52 @@
 //For numbers divisible by 3 and 5 we want to print fizzbuzz
 $(document).ready(function(){
 
+	var userNum = function(){
+		return Number(prompt("Please choose a whole number between 1 and 100","Type number here!"));
+	};
 
-	for (var count = 1; count <= 100; count++){
-		if( count % 3 === 0 && count % 5 === 0){
-			$("body").append("FizBuzz").append("<br />");
-		} else if(count % 3 === 0){
-			$("body").append("Fiz").append("<br />");
-		} else if(count % 5 === 0){
-			$("body").append("Buzz").append("<br />");
-		} else {
-			$("body").append(count).append("<br />");
+	var userInput = userNum();
+
+		var play = function() {
+		for( var count = 1; count <= userInput; count++){
+			if( count % 3 === 0 && count % 5 === 0){
+				$("body").append("FizBuzz").append("<br />");
+			} else if(count % 3 === 0){
+				$("body").append("Fiz").append("<br />");
+			} else if(count % 5 === 0){
+				$("body").append("Buzz").append("<br />");
+			} else {
+				$("body").append(count).append("<br />");
+			}
 		}
-	}
+	} // end of play
 
-})
 
+		var game = function() {
+			if(userInput <= 0 || userInput >100){
+				alert("Please choose a number between 1 and 100");
+			}else if (userInput % 1 != 0){
+				alert("Please choose a WHOLE number between 1 and 100");
+			} else {
+				// alert("Lets run the program!");
+				play();
+			}
+		
+		}; //end of game
+
+
+game();
+
+
+}); // end of doc ready
+					
+
+
+
+// if(userInput <= 0 || userInput >100){
+// 			alert("Please choose a number between 1 and 100");
+// 		} else if (userInput %1 != 0) {
+// 			alert("Please choose a WHOLE number between 1 and 100");
+// 		} else {
+// 			alert("Nice Pick");
+// 		}
