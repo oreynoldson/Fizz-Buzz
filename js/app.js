ea4b4b911cd65470c8e5ffa5da011888c8e839ce
oreynoldson <oli.reynoldson@gmail.com>
2014-08-28 18:38:36 -0700

    While loop Bug Fixed
    Added userInput as condition to the functions

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 //For numbers divisible by 3 and 5 we want to print fizzbuzz
 $(document).ready(function(){
 
- var play = function() {
+ var play = function(userInput) {
  	 
  	for( var count = 1; count <= userInput; count++){
  		if( count % 3 === 0 && count % 5 === 0){
@@ -23,7 +23,7 @@ $(document).ready(function(){
  	var userInput = Number(prompt("Please choose a whole number between 1 and 100","Type number here!"));
  	 
  	if(userInput > 0 && userInput <= 100 && !isNaN(userInput)){
- 	play();
+ 	play(userInput);
  	} else {
  	alert("Please choose a WHOLE number between 1 and 100");
  	checkValue();
